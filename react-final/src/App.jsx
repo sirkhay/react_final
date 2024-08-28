8569130879eb71fe71ba95298e3da9a5cf69c13f
Kehinde Bashorun <sirkennybashon@gmail.com>
2024-08-28 23:53:32 +0100

    first commit

---
+++ react-final/src/App.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppHeader from "./components/Header";
+
+function App() {
+  return (
+    <div className='App'>
+      <header id='header'>
+        <AppHeader />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-final/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+
+export default function AppHero() {
+  return (
+    <div>
+      <div className={styles.hero}>
+        <h1>Creating the Future with DesignHub</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
+          quia. Reprehenderit debitis sunt suscipit facere quod asperiores
+          perferendis fugiat culpa.
+        </p>
+      </div>
+    </div>
+  );
+}
